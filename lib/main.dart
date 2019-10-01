106fc7752574cf25382c1210cf71b59a1cc4d3cb
luisvega1 <luis.vegal@hotmail.com>
2019-09-30 19:07:58 -0600

    app funcional

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumer = 1;
+  int rightDiceNumer = 2;
+  @override
+  void random() {
+    leftDiceNumer = Random().nextInt(6) + 1;
+    rightDiceNumer = Random().nextInt(6) + 1;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+                onPressed: (){
+                  setState(() {
+                    random();
+                  });
+                  print('left button got pressed.');
+                },
+                child: Image.asset('images/dice$leftDiceNumer.png'),
+              )
+          ),
+          Expanded(
+              child: FlatButton(
+                onPressed: (){
+                  setState(() {
+                    random();
+                  });
+                  print('Rigth button got pressed.');
+                },
+                child: Image.asset('images/dice$rightDiceNumer.png'),
+              )
+          ),
+        ],
+      ),
+    );
   }
 }
+
